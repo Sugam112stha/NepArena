3bdb058897d101ef8b0ab0c5762a3149a9e7fd5d
Sugam Shrestha <sugamstha619@gmail.com>
2026-08-12 12:09:04 +0545

    making a nav bar with responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,34 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo/logo1.png";
-const Navbar = () => {
-    const navItems = [
-        {name: "Home", path: "/"},
-        {name: "About", path: "/about"},
-        {name: "Tournaments", path:"/tournaments"},
-        {name: "Leaderboard", path: "/leaderboard"},
-        {name: "Contact", path: "/contact"},
-    ]
+import { useState } from "react";
+const Navbar = () => { 
+    const[isOpen, setIsOpen] = useState(false);
+    
+const navItems = [
+    { name: "About", path: "/about" },
+    { name: "Tournaments", path: "/tournaments" },
+    { name: "Leaderboard", path: "/leaderboard" },
+    { name: "Contact", path: "/contact" },
+  ];
   return (
-    <nav>
-        <Link to="/">
-        <img src={logo} alt="NepArena" className="h-12 w-auto"/>
+    <nav className="fixed top-0 left-0 z-50 w-full border-b border-white/10 bg-black/60 backdrop-blur-md">
+      <div className="mx-auto max-w-7xl flex h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
+
+        <Link to="/" onClick={() => setIsOpen}>
+          <img src={logo} alt="NepArena" className="h-12 w-auto sm:h-14" />
         </Link>
 
-        {navItems.map((items) => (
-            <Link key={items.name} to={items.path}>
-                {items.name}
-            </Link>
+       <div className="hidden items-center gap-6 lg:flex">
+         {navItems.map((items) => (
+          <Link key={items.name} to={items.path} className="text-sm text-gray-300 transition hover:text-[#E50914]">
+            {items.name}
+          </Link>
         ))}
+       </div>
+       
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
