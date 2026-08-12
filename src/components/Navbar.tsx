ef387d5c599cab1c0c82ab828479cbf35769d1b6
Sugam Shrestha <sugamstha619@gmail.com>
2026-08-12 13:11:05 +0545

    Nav Bar finish

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo/logo1.png";
 import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
 const Navbar = () => { 
     const[isOpen, setIsOpen] = useState(false);
     
 const navItems = [
+    { name: "Home", path: "/home"},
     { name: "About", path: "/about" },
     { name: "Tournaments", path: "/tournaments" },
     { name: "Leaderboard", path: "/leaderboard" },
@@ -27,16 +29,49 @@ const navItems = [
        </div>
 
        <div className="hidden items-center gap-3 lg:flex">
-        <Link to = "/login" className="px-4 py-2 text-sm text-gray-300 transition hover:text-white hover:scale-105">
+        <Link to = "/login" className="px-4 py-2 text-sm bg-[#E50914] text-gray-300 font-semibold rounded-lg transition hover:text-white hover:scale-105 hover:bg-[#FF1E2D]">
             Login
         </Link>
 
-        <Link to="/signup" className="px-4 py-2 text-sm text-gray-300 transition hover:text-white hover:scale-105">
+        <Link to="/signup" className="px-4 py-2 text-sm bg-[#E50914] text-gray-300 font-semibold rounded-lg transition hover:text-white hover:scale-105 hover:bg-[#FF1E2D]">
             Sign Up
         </Link>
        </div>
 
+       <button onClick={() => setIsOpen(!isOpen)} 
+       className="rounded-lg p-2 text-2xl text-white lg:hidden"
+        aria-label = "Toggle navigation menu">
+        {isOpen ? <FiX /> : <FiMenu />}
+       </button>
+
       </div>
+
+      {isOpen && (
+        <div className="border-t border-white/10 text-white bg-[#0D0D0D] px-4 py-10 lg:hidden">
+            <div className="flex flex-col gap-5">
+                {navItems.map((item) => (
+                    <Link key={item.name} to={item.path} onClick={() => setIsOpen(false)}>
+                        {item.name}
+                    </Link>
+                ))}
+            <div className="my-2 border-t border-white/10" />    
+            <Link
+              to="/login"
+              onClick={() => setIsOpen(false)}
+              className="mt-1 rounded-lg bg-[#E50914] px-4 py-3 text-center text-sm font-semibold text-white transition hover:bg-[#FF1E2D]">
+              Login
+            </Link>
+
+            <Link
+              to="/signup"
+              onClick={() => setIsOpen(false)}
+              className="mt-1 rounded-lg bg-[#E50914] px-4 py-3 text-center text-sm font-semibold text-white transition hover:bg-[#FF1E2D]">
+              Sign Up
+            </Link>
+
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
