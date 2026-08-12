fe204d9d69cbb19a3f43ce5329862d482e9ccd76
Sugam Shrestha <sugamstha619@gmail.com>
2026-08-12 13:30:11 +0545

    connect routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import About from './pages/About';
+import Leaderboard from './pages/Leaderboard';
+import Tournaments from './pages/Tournaments';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Navbar />
+
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/tournaments" element={<Tournaments />}/>
+      <Route path="/leaderboard" element={<Leaderboard />}/>
+      <Route path="/contact" element={<Contact />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<SignUp />}/>
+    </Routes>
+
     </BrowserRouter>
   )
 }
