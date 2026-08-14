d8a1e10a42e67bff11fa74319275ae124c765a47
Sugam Shrestha <sugamstha619@gmail.com>
2026-08-14 09:04:55 +0545

    button to become a partner has added

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom"
+
 const Partner = () => {
+    const navigate = useNavigate();
   return (
     <section className="bg-[#050505] px-4 py-16 text-white sm:px-6 sm:py-20">
         <div className="mx-auto max-w-6xl">
@@ -8,12 +11,18 @@ const Partner = () => {
                 </p>
 
                 <h2 className="mt-3 font-bold text-3xl sm:text-4xl">
-                    Supported By
+                    Grow Esport With NepArena
                 </h2>
 
                 <p className="mx-auto mt-4 max-w-2xl text-sm text-gray-400 sm:text-base">
-                    Working together to grow the esports community in Nepal.
+                    Partner with NepArena to support competitive esports and help
+                    create more opportunities for teams and players across Nepal.
                 </p>
+
+                <button onClick={() => navigate("/Contact")}
+                    className="mt-8 rounded-lg bg-[#E50914] px-6 py-3 text-sm font-semibold transition hover:bg-[#ff1e2d]">
+                    Become a Partner
+                </button>
             </div>
         </div>
     </section>
